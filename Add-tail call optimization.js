915910296fdc8e87b76a2de1a05bd0fd749b9e7e
sweetytang <tanglibai520@gmail.com>
2019-08-24 17:48:21 +0800

    init

--- Add-tail call optimization.js
+++ Add-tail call optimization.js
@@ -1,10 +1,10 @@
 
   var value;                                                  //下一个递归函数
-  var active = false;                                  //递归信号
-  var accumulated = [];                         //复制arguments的参数，不然下一个函数调用参数时候还需要参考上一个函数的arguments参数，那么上一个函数的调用帧一直存在
+  var active = false;                                  //递归开始信号，只有第一次的sum可以开始，后面的sum只为了将参数写入accumulated（因为active的原因），随后变成循环，
+  var accumulated = [];                         //复制arguments的参数，这是循环进行的信号
 
   function f(x, y) {
-    console.trace();
+    
     if (y > 0) {
       return sum(x + 1, y - 1)                  //返回的是sum函数
     }
@@ -12,15 +12,17 @@
   }
 
    function sum() {
-    
+    console.log('2');
     accumulated.push(arguments);
     if (!active) {
       active = true;
+      console.log('3');
       while (accumulated.length) {
+        console.log('4')
         value = f.apply(this, accumulated.shift());
       }
-      active = false;
-      return value;
+      active = false;                                 //初始化，为了下次递归优化
+      return value;                                  //返回最终的计算值
     }
   }
 
